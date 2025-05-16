17c1bca466d98c26a7c2a47ed156638c63e9a75e
Lucas Tameirão <lucas.atmartins@gmail.com>
2025-05-15 21:07:37 -0300

    função listagem de filmes

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -1,54 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
+const container = document.getElementById("filmes-container");
 
-    const form = document.getElementById('book-form');
-    const  bookList = document.getElementById('book-list');
 
-    listarLivros();
+function listarFilmes(){
 
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
+    fetch("http://localhost:3000/filme").then(response => response.json()).then(listaFilmes => {
 
-        const livro = {
-            titulo: document.getElementById('title').value,
-            autor: document.getElementById('author').value,
-            nota: document.getElementById('rating').value,
-            data: document.getElementById('date').value,
-            comentario: document.getElementById('review').value
-        }
+        const div = document.createElement('div');
 
-        fetch('http://localhost:3000/livros', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(livro),
-        }).then(response => response.json()).then(() => {
-            listarLivros();
-            form.reset();
-        }).catch(error => {
-            console.error('Erro ao adicionar o livro:', error);
-          });
-    })
-
-    function listarLivros(){
-        fetch('http://localhost:3000/livros').then(response => response.json()).then(livros => {
-            bookList.innerHTML = '';
-
-            livros.forEach(livro => {
-                const div = document.createElement('div');
-                div.classList.add('livro-item');
-
-                div.innerHTML = `
-                    <h3>${livro.titulo}</h3>
-                    <p><strong>Autor:</strong> ${livro.autor}</p>
-                    <p><strong>Nota:</strong> ${livro.nota}</p>
-                    <p><strong>Data:</strong> ${livro.data}</p>
-                    <p><strong>Comentário:</strong> ${livro.comentario}</p>
-                    <hr>
-                `
-
-                bookList.appendChild(div);
-            });
+        listaFilmes.forEach(filme => {
+            const image = document.createElement('img');
+            image.src = filme.imagem;
+            image.classList.add('capa-filme');
+            div.appendChild(image);
+            container.appendChild(div);
         });
-    }
-});
+    })
+}
+
+listarFilmes();
